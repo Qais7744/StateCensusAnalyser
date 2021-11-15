8d91a0a68e9aff02e12eded13c8c1b5ec6d8f413
unknown <altamashbagwan1@gmail.com>
2021-11-15 07:07:37 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bl/designpriciples/welcomeMessgae.java
@@ -0,0 +1,7 @@
+package com.bl.designpriciples;
+
+public class welcomeMessgae {
+    public static void main(String[] args) {
+        System.out.println("Welcome Indian State Analyser Problem");
+    }
+}
